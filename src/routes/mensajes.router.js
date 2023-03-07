a64d40114f46a12e8d0377693af1be7072e3cdcb
Alejandro <alejandrorh295@hotmail.com>
2023-03-07 00:29:28 -0300

    small fixes, router.mensajes jwt auth, packagejson

--- src/routes/mensajes.router.js
+++ src/routes/mensajes.router.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import passport from "../middlewares/Passport.js";
 
 const router = Router();
 
 import { globalChat, personalChat } from "../controllers/mensajesController.js";
 
-router.get("/chat", globalChat);
-router.get("/chat/:email", personalChat);
+router.get(
+  "/chat",
+  passport.authenticate("jwt", { session: false }),
+  globalChat
+);
+router.get(
+  "/chat/:email",
+  passport.authenticate("jwt", { session: false }),
+  personalChat
+);
 
 export default router;
